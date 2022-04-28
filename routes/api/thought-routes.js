d9eb7f56621b900c4073e2da52d75f7b4ca8c1e6
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-28 16:36:22 -0700

    completed thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+///// /api/thought /////
+router.route('/')
+        .get(getAllThoughts)
+        .post(createThought);
+
+
+///// /api/thought/:thoughtId
+router.route('/:/id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+
+///// /api/thought/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+    .post(addReaction);
+
+
+
+///// /api/thought/:thoughtId/reactions/:reactionId /////
+router.route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
+
+
+
+module.exports = router;
