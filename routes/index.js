f72e217b427c322b954f40eefec904d7a5b7c97e
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-28 16:39:59 -0700

    fixed syntax error in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const thoughtRoutes = requires('./thought-routes');
+const thoughtRoutes = require('./thought-routes');
 const userRoutes = require('./user-routes');
 
 
