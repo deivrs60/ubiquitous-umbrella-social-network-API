da5c49831970ab0ee2276026a720ebef5bfb3dc8
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-27 23:57:40 -0700

    User model created

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: [true, 'Please enter your username'],
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, 'Please enter your email'],
+            match: [/.+\@.+\..+/, 'is not valid']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        // prevent duplicate of _id as 'id'
+        id: false
+    }
+);
+
+// get total count of friends on retrieval 
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+//export user module
+module.exports = User;
+
