3186ddf2128af5729fe58f350580267a4b932e36
David Reyes Santoyo <deiv@Deivs-MBP.local>
2022-04-28 15:45:53 -0700

    user routes completed

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -9,3 +9,25 @@ const {
     addFriend,
     deleteFriend,
 } = require('../../controllers/user-controller');
+
+
+///// api/users /////
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+///// /api/users/:userId /////
+
+router.route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+////// /api/users/:userId/friends/:friendId
+
+router.route('/users/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+
+module.exports = router;
